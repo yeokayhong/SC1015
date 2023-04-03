5ff30429d9809a5ffb1d3e021748ac47fc34295f
Yeo Kay Hong <51885114+yeokayhong@users.noreply.github.com>
2023-04-03 21:10:09 +0800

    updates

--- project.py
+++ project.py
@@ -12,6 +12,9 @@ bike_data = pandas.read_csv("data/all_bikez_curated.csv")
 bike_data = bike_data[bike_data["Power (hp)"].notnull()]
 bike_data["Stroke (mm)"] = bike_data["Stroke (mm)"].apply(lambda x: x.replace(",", "") if type(x) != float else x).astype("float64")
 bike_data = bike_data.drop(["Model", "Fuel system", "Front brakes", "Rear brakes", "Front tire", "Rear tire", "Front suspension", "Rear suspension", "Color options"], axis=1)
+bike_data = bike_data[bike_data["Torque (Nm)"].notnull()]
+bike_data = bike_data[bike_data["Displacement (ccm)"].notnull()]
+bike_data = bike_data[bike_data["Cooling system"].notnull()]
 
 bike_data.info()
 bike_data.describe()
@@ -39,6 +42,14 @@ plt.show()
 seaborn.pairplot(bike_data, x_vars="Power (hp)")
 plt.show()
 
-print(bike_data.corr(numeric_only=True)["Power (hp)"].sort_values(ascending=False))
+print(bike_data.corr()["Power (hp)"].sort_values(ascending=False))
+
+# %%
+# one hot encode Cooling System
+bike_data = pandas.get_dummies(bike_data, columns=["Cooling system"], prefix=["Cooling system"])
+bike_data = bike_data.reset_index(drop=True)
+
+x = bike_data.loc[:, ["Cooling system_Air", "Cooling system_Liquid", "Cooling system_Oil & air", "Torque (Nm)", "Displacement (ccm)"]]
+y = bike_data["Power (hp)"]
 
 # %%
